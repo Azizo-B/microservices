113919fcb5f9dbd236794c6e2e7bc7a4471ed064
Yharnam <demirogullariraif@gmail.com>
2025-01-04 01:59:52 +0100

    [FEATURE] Enhance user account service with CRUD operations and validation

--- user-service/src/service/userAccount.service.ts
+++ user-service/src/service/userAccount.service.ts
@@ -1,36 +1,60 @@
 import { UserAccount } from "@prisma/client";
 import { prisma } from "../data";
 import handleDBError from "./_handleDBError";
+import { CreateUserAccountInput, UpdateUserAccountInput } from "../types/userAccount.types";
+import { checkPermission } from "./user.service";
+import ServiceError from "../core/serviceError";
 
-export async function createUserAccount(username: string, password: string, status: string, userId: string, appId: string ): Promise<UserAccount> {
-    try{
-        return await prisma.userAccount.create({ data: {
-            username, password, status, userId, appId,
-         }
-        });
-    }
-    catch(error){
-        handleDBError(error);
-    }
+
+// userAccountService.ts
+export async function createUserAccount(
+    username: string, 
+    passwordHash: string, 
+    status: string, 
+    userId: string, 
+    appId: string
+): Promise<UserAccount> {
+    const userAccount = await prisma.userAccount.create({
+        data: {
+            username,
+            passwordHash, 
+            status,
+            userId,
+            appId
+        }
+    });
+
+    return userAccount;
 }
 
-export async function getAllUserAccounts(): Promise<UserAccount[]>{
-    const userAccounts = await prisma.userAccount.findMany();
+
+export async function getAllUserAccounts(requestingUserId: string, userId: string): Promise<UserAccount[]>{
+    userId= userId === "me" ? requestingUserId : userId;
+    
+      if (userId !== requestingUserId && ! await checkPermission("userservice:list:any:useraccounts", requestingUserId)) {
+        throw ServiceError.notFound("User not found"); // Do not leak user information
+      }
+    
+
+    const userAccounts = await prisma.userAccount.findMany({where: {id: userId} });
     return userAccounts;
+
 }
+
 
 export async function getUserAccountById(id: string): Promise<UserAccount | null>{
     const userAccount = await prisma.userAccount.findUnique({where: {id}});
     return userAccount;
 }
 
-export async function updateUserAccount(id: string, username: string, password: string, status: string): Promise<UserAccount>{
-    const userAccount = await prisma.userAccount.update({where: {id}, data: {username, password, status}}); //idk of ik userid en appid ook moe update
+export async function updateUserAccount(id: string, UpdateUserAccountInput: UpdateUserAccountInput): Promise<UserAccount>{
+    const {username, status} = UpdateUserAccountInput;
+    const userAccount = await prisma.userAccount.update({where: {id}, data: {username, status}}); //idk of ik userid en appid ook moe update
     return userAccount; 
 }
 
 export async function deleteUserAccount(id:string): Promise<UserAccount> {
-    const userAccount = await prisma.userAccount.delete({where: {id}});
+    const userAccount = await prisma.userAccount.update({where: {id}, data: {status: 'inactive'}});
     return userAccount;
 }
 
