453e64aede9d695f0c62878925044b66e27b341d
Azizo-B <aziz.baatout@gmail.com>
2025-01-12 14:33:32 +0100

    [FEATURE] Implement CI/CD

--- user-service/tests/helpers/withServer.ts
+++ user-service/tests/helpers/withServer.ts
@@ -20,7 +20,7 @@ export default function withServer(setter: (s: supertest.Agent) => void): void {
 
   beforeAll(async () => {
     server = await createServer();
-    await cleanUp()
+    await cleanUp();
 
     await prisma.application.create({data:{id:"507f191e810c19729de860ea", name:"Test Application"}});
 
@@ -119,7 +119,7 @@ export default function withServer(setter: (s: supertest.Agent) => void): void {
 
   afterAll(async () => {
     // Cleanup database and stop the server
-    await cleanUp()
+    await cleanUp();
     await server.stop();
   }, 30000);
 }
